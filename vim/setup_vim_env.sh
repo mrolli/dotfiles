91acca5cc6b0e5bb6bc48dcaac51bd63d9169ebb
Michael Rolli <michael@rollis.ch>
2014-12-15 13:00:26 +0100

    Ruby plugins added.

--- vim/setup_vim_env.sh
+++ vim/setup_vim_env.sh
@@ -24,22 +24,37 @@ echo -e "... done.\n"
 # setup bundles
 echo "Now setting up bundles..."
 cd ~/.vim/bundle
-[ -d nerdtree ] || git clone https://github.com/scrooloose/nerdtree.git
-[ -d supertab ] || git clone git://github.com/ervandew/supertab.git
-[ -d syntastic ] || git clone https://github.com/scrooloose/syntastic.git
-[ -d tabular ] || git clone git://github.com/godlygeek/tabular.git
-[ -d vim-airline ] || git clone git://github.com/bling/vim-airline.git
-[ -d vim-signify ] || git clone https://github.com/mhinz/vim-signify ~/.vim/bundle/vim-signify
-[ -d vim-fugitive ] || git clone git://github.com/tpope/vim-fugitive.git
-#[ -d vim-phpcomplete ] || git clone github.com/shawncplus/phpcomplete.vim.git vim-phpcomplete
-[ -d vim-puppet ] || git clone git://github.com/rodjek/vim-puppet.git
-[ -d vim-sensible ] || git clone git://github.com/tpope/vim-sensible.git
-#[ -d tlib_vim ] || git clone https://github.com/tomtom/tlib_vim.git # dependency of snnipmate
-#[ -d vim-addon-mw-utils ] || git clone https://github.com/MarcWeber/vim-addon-mw-utils.git # dependency of snnipmate
+[ -d nerdtree ]     || git clone https://github.com/scrooloose/nerdtree.git
+[ -d syntastic ]    || git clone https://github.com/scrooloose/syntastic.git
+[ -d supertab ]     || git clone git://github.com/ervandew/supertab.git
+[ -d tabular ]      || git clone git://github.com/godlygeek/tabular.git
+[ -d vim-airline ]  || git clone git://github.com/bling/vim-airline.git
+[ -d vim-signify ]  || git clone https://github.com/mhinz/vim-signify ~/.vim/bundle/vim-signify
 [ -d snipmate.vim ] || git clone git://github.com/ervandew/snipmate.vim.git
 [ -d snipmate.vim/snippets ] && rm -rf snipmate.vim/snippets # conflicts with below vim-snippets if directory is available
 [ -d vim-snippets ] || git clone https://github.com/honza/vim-snippets.git
-[ -d vim-surround ] || git clone git://github.com/tpope/vim-surround.git
+
+# tpope stuff
+[ -d vim-fugitive ]   || git clone git://github.com/tpope/vim-fugitive.git
+[ -d vim-git ]        || git clone git://github.com/tpope/vim-git.git
+[ -d vim-markdown ]   || git clone git://github.com/tpope/vim-markdown.git
+[ -d vim-repeat ]     || git clone git://github.com/tpope/vim-repeat.git
+[ -d vim-sensible ]   || git clone git://github.com/tpope/vim-sensible.git
+[ -d vim-surround ]   || git clone git://github.com/tpope/vim-surround.git
+[ -d vim-vividchalk ] || git clone git://github.com/tpope/vim-vividchalk.git
+
+# Ruby related
+[ -d vim-ruby ]          || git clone git://github.com/vim-ruby/vim-ruby.git
+[ -d vim-ruby-debugger ] || git clone git://github.com/astashov/vim-ruby-debugger.git
+[ -d vim-rdoc ]          || git clone git://github.com/hallison/vim-rdoc.git
+[ -d vim-cucumber ]      || git clone git://github.com/tpope/vim-cucumber.git
+[ -d vim-rails ]         || git clone git://github.com/tpope/vim-rails.git
+[ -d Gist.vim ]          || git clone git://github.com/vim-scripts/Gist.vim.git
+
+# Puppet related
+[ -d vim-puppet ] || git clone git://github.com/rodjek/vim-puppet.git
+
+
 echo -e "All bundles setup.\n"
 
 cd $OLDPWD
