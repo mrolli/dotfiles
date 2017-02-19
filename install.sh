23653562b944d343eeb3e25c0cbc8bfbb109652f
Michael Rolli <michael@rollis.ch>
2017-02-19 11:49:48 +0100

    Moved to holman style install script.
    The script manages symlinks and installs fonts. Or should the later be a
    topic install script instead? Let's leave it herein of the moment.

--- install.sh
+++ install.sh
@@ -1,19 +1,144 @@
 #!/usr/bin/env bash
+#
+# bootstrap installs dotfiles and
+# runs every installer found..
+#
 
-cd ~
-# Link in files, replacing whatever's already there.
-ln -fs ".dotfiles/bash/bashrc"                "${HOME}/.bashrc"
-ln -fs ".dotfiles/bash/bash_profile"          "${HOME}/.bash_profile"
-ln -fs ".dotfiles/git/gitconfig"              "${HOME}/.gitconfig"
-ln -fs ".dotfiles/git/gitmessage"             "${HOME}/.gitmessage"
-ln -fs ".dotfiles/git/gitignore_global"       "${HOME}/.gitignore_global"
-ln -fs ".dotfiles/tmux/tmux.conf"             "${HOME}/.tmux.conf"
-ln -fs ".dotfiles/vim/vimrc"                  "${HOME}/.vimrc"
+cd "$(dirname "$0")"
+DOTFILES_ROOT_ABS=$(pwd -P)
+DOTFILES_ROOT_REL=${DOTFILES_ROOT_ABS#$HOME/}
+cd $HOME
 
-# Link in directories, removing whatever's already there first.
-[[ -e "${HOME}/.vim" ]] && rm -r "${HOME}/.vim"
-ln -fs ".dotfiles/vim" "${HOME}/.vim"
+FONT_DIR=$HOME/Library/Fonts
 
-# Finally setup vim plugins and color schemes
-~/.vim/setup_vim_env.sh
+set -e
+
+echo ''
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+
+link_file () {
+  local src=$1 dst=$2
+
+  local overwrite= backup= skip=
+  local action=
+
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]; then
+    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]; then
+
+      local currentSrc="$(readlink $dst)"
+
+      if [ "$currentSrc" == "$src" ]; then
+        skip=true;
+      else
+        user "File already exists: $dst ($(basename "$src")), what do you want to do?\n\
+        [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+        read -n 1 action
+
+        case "$action" in
+          o )
+            overwrite=true;;
+          O )
+            overwrite_all=true;;
+          b )
+            backup=true;;
+          B )
+            backup_all=true;;
+          s )
+            skip=true;;
+          S )
+            skip_all=true;;
+          * )
+            ;;
+        esac
+      fi
+    fi
+
+    overwrite=${overwrite:-$overwrite_all}
+    backup=${backup:-$backup_all}
+    skip=${skip:-$skip_all}
+
+    if [ "$overwrite" == "true" ]; then
+      rm -rf "$dst"
+      success "Successfully removed $dst"
+    fi
+
+    if [ "$backup" == "true" ]; then
+      mv "$dst" "${dst}.backup"
+      success "Successfully moved $dst to ${dst}.bak"
+    fi
+
+    if [ "$skip" == "true" ]; then
+      success "Skipped $src"
+    fi
+  fi
+
+  if [ "$skip" != "true" ]; then  # "false" or empty
+    ln -s "$1" "$2"
+    success "Successfully symlinked $1 to $2"
+  fi
+}
+
+install_dotfiles () {
+  info 'Installing dotfiles...'
+
+  local overwrite_all=false backup_all=false skip_all=false
+
+  for src in $(find -H "$DOTFILES_ROOT_REL" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
+  do
+    dst="$HOME/.$(basename "${src%.*}")"
+    link_file "$src" "$dst"
+  done
+}
+
+install_fonts () {
+  info 'Installing fonts...'
+
+  if [ -d "$DOTFILES_ROOT_REL/fonts" ]
+  then
+    cd "$DOTFILES_ROOT_REL/fonts"
+
+    SAVEIFS=$IFS
+    IFS=$'\n'
+    for ttf_file in $(find -H . -name '*.ttf')
+    do
+      if [ -f "$FONT_DIR/$ttf_file" ]
+      then
+        success "Skipped $ttf_file"
+      else
+        cp "$ttf_file" "$FONT_DIR/$ttf_file"
+        success "Successfully installed $ttf_file"
+      fi
+    done
+    # restore $IFS
+    IFS=$SAVEIFS
+    cd $HOME
+  fi
+}
+
+install_dotfiles
+echo ''
+install_fonts
+
+echo ''
+echo '  Everything in place, have fun!'
+
+exit 0
 
