47bc37da435b194221825e8e5da2324585142ba4
Michael Rolli <michael@rollis.ch>
2017-02-26 09:10:29 +0100

    First sketch for Homebrew/holeman style installers

--- terminalapp/install.sh
+++ terminalapp/install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-open "Dark Background.terminal"
+topicpath=$(dirname $0)
+
+open "$topicpath/Dark Background.terminal"
 osascript -e 'tell application "Terminal" to close window 0'
 defaults write com.apple.Terminal "Default Window Settings" -string "Dark Background"
 defaults delete com.apple.Terminal "Startup Window Settings"
