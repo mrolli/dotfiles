11139c22ee507355ae0278605788d97b8744a647
Michael Rolli <michael@rollis.ch>
2017-03-04 09:28:27 +0100

    Added code to change hostname on macOS.

--- macos/install.sh
+++ macos/install.sh
@@ -3,8 +3,27 @@
 topic_dir=$(dirname $0)
 source $topic_dir/../shell_functions.sh
 
-# Invoke softwareupdate from CLI
+doupdate=1
 echo ''
-info "sudo softwareupdate -i -a"
-sudo softwareupdate -i -a
+prompt_confirm "Do you want me to run softwareupdate?" || doupdate=0
+if [ $doupdate -eq 1 ]
+then
+  # Invoke softwareupdate from CLI
+  info "sudo softwareupdate -i -a"
+  sudo softwareupdate -i -a
+fi
+
+chhostname=1
+echo ''
+prompt_confirm "Do you want me to change the hostname of this machine?" || chhostname=0
+if [ $chhostname -eq 1 ]
+then
+  user "Which short hostname should I set? "
+  read -r myhostname
+  sudo scutil --set ComputerName "${myhostname}"
+  sudo scutil --set HostName "${myhostname}"
+  sudo scutil --set LocalHostName "${myhostname}"
+  sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "${myhostname}"
+  success "Hostname set to ${myhostname}."
+fi
 
--- shell_functions.sh
+++ shell_functions.sh
@@ -1,11 +1,23 @@
 # Generals funciton used in shell scripts
 
+prompt_confirm() {
+  while true; do
+    printf "\r  [ \033[0;33m??\033[0m ] ${1:-Continue?} [y/n]: "
+    read -r -n 1 REPLY
+    case $REPLY in
+      [yY]) echo ; return 0 ;;
+      [nN]) echo ; return 1 ;;
+      *) printf " \033[31m %s \n\033[0m" "invalid input"
+    esac
+  done
+}
+
 info () {
   printf "\r  [ \033[00;34m..\033[0m ] $1\n"
 }
 
 user () {
-  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+  printf "\r  [ \033[0;33m??\033[0m ] $1"
 }
 
 success () {
