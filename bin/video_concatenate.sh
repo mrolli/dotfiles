da37e21ca028189d19fec8c589f5c007696a2461
Michael Rolli <michael@rollis.ch>
2020-10-09 12:14:04 +0200

    New script for concatenating videos using ffmpeg

---
+++ bin/video_concatenate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 2 ]
+then
+  echo "Usage: ${0} fileext targetfilename"
+  exit 1
+fi
+
+type=$1
+outfile="$2"
+
+for f in ./*$type; do echo "file '$f'" >> .filelist.txt; done
+ffmpeg -f concat -safe 0 -i .filelist.txt -c copy "${outfile}"
+rm -f .filellist.txt
