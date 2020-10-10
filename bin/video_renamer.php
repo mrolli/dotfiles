ca51655ee8a5e3025b04d85ca80d81efc885c268
Michael Rolli <michael@rollis.ch>
2020-10-10 09:44:10 +0200

    Differentate between files and dirs

--- bin/video_renamer.php
+++ bin/video_renamer.php
@@ -19,11 +19,17 @@ foreach ($filenames as $origfilename) {
     if ($origfilename == 'ranking.txt') {
         continue;
     }
-    $ext = strtolower(pathinfo($origfilename, PATHINFO_EXTENSION));
-    $filename = pathinfo($origfilename, PATHINFO_FILENAME);
+
+    if (is_dir($origfilename)) {
+        $ext = '';
+        $filename = $origfilename;
+    } else {
+        $ext = strtolower(pathinfo($origfilename, PATHINFO_EXTENSION));
+        $filename = pathinfo($origfilename, PATHINFO_FILENAME);
+    }
     $filename = preg_replace(array_keys($pregReplaceStruct), array_values($pregReplaceStruct), $filename);
     $filename = ucwords($filename, " .,\t\r\n\f\v");
-    $newfilename = $filename . '.' . $ext;
+    $newfilename = empty($ext) ? $filename : $filename . '.' . $ext;
     if ($origfilename !== $newfilename) {
         if ($notest) {
             rename($origfilename, $newfilename);
