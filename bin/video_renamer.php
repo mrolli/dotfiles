f34d5ade4765292eb8b6d302a3af9bd2d38b7043
Michael Rolli <michael@rollis.ch>
2020-10-10 08:32:15 +0200

    New replaces

--- bin/video_renamer.php
+++ bin/video_renamer.php
@@ -4,6 +4,9 @@
 $pregReplaceStruct = array(
     //    '/Do.*\.S02E/' => 'Doctor.Who.S02E',
     '/-/'    => '',
+    '/,/'    => '',
+    '/Sc\./'    => 'Scene.',
+    '/\d+\.\s+/'    => '',
     '/ +/'   => '.',
     '/[()]/' => '',
 );
@@ -13,8 +16,11 @@ $notest = isset($argv[1]) && $argv[1] == "-n" ? true : false;
 $filenames = glob('*');
 
 foreach ($filenames as $origfilename) {
+    if ($origfilename == 'ranking.txt') {
+        continue;
+    }
     $newfilename = preg_replace(array_keys($pregReplaceStruct), array_values($pregReplaceStruct), $origfilename);
-    $newfilename = ucwords($newfilename);
+    $newfilename = ucwords($newfilename, " .,\t\r\n\f\v");
     if ($origfilename !== $newfilename) {
         if ($notest) {
             rename($origfilename, $newfilename);
