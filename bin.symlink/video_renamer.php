c5fe633ab05661d0c50ed608fefd80155a92c395
Michael Rolli <michael@rollis.ch>
2021-04-14 09:20:44 +0200

    Cleanup

--- bin.symlink/video_renamer.php
+++ bin.symlink/video_renamer.php
@@ -2,15 +2,20 @@
 <?php
 
 $pregReplaceStruct = array(
-    '/ - /' => '.',
-    '/[!=\'"\[\]\(\)],/' => '',
-    '/ +/'   => '.',
-    '/&/' => 'und',
-    '/\.$/' => '',
-    '/„/' => 'ä',
-    '/則/' => 'ä',
-    '/派/' => 'ö',
-    '/�/' => 'ü'
+    '/ - /'              => '.',
+    '/, /'               => '.',
+    '/,/'                => '.',
+    '/ +/'               => '.',
+    '/\.$/'              => '',
+    '/[!=\'"’\[\]\(\)]/' => '',
+    '/ä/'                => 'ae',
+    '/ö/'                => 'oe',
+    '/ü/'                => 'ue',
+    '/&/'                => 'and',
+    '/„/'                => 'ä', # KPT
+    '/則/'               => 'ä', # KPT
+    '/派/'               => 'ö', # KPT
+    '/�/'                => 'ü' # KPT
 );
 
 $notest = isset($argv[1]) && $argv[1] == "-n" ? true : false;
@@ -30,6 +35,7 @@ foreach ($filenames as $origfilename) {
         $filename = pathinfo($origfilename, PATHINFO_FILENAME);
     }
     $filename = preg_replace(array_keys($pregReplaceStruct), array_values($pregReplaceStruct), $filename);
+    # DO NOT add strtolower here as it would break acronyms
     $filename = ucwords($filename, " .,\t\r\n\f\v");
     $newfilename = empty($ext) ? $filename : $filename . '.' . $ext;
     if ($origfilename !== $newfilename) {
