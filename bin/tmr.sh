691229e3b422b8ec3ea9914792409aece1220900
Michael Rolli <michael@rollis.ch>
2021-05-04 08:50:49 +0200

    More standard windows

--- bin/tmr.sh
+++ bin/tmr.sh
@@ -1,44 +1,63 @@
 #!/bin/sh
 
-SESSION_NAME="$(hostname -s)"
+session_name="$(hostname -s)"
 
-tmux has-session -t ${SESSION_NAME} >/dev/null 2>&1
+tmux has-session -t ${session_name} >/dev/null 2>&1
 
 if [ $? != 0 ]
 then
-  if [[ "${SESSION_NAME}" = "rivendell" ]]
+  if [[ "${session_name}" = "rivendell" ]]
   then
-    PUP_DIR=$HOME/Developer/ubelix/puppet-installer/controlrepo
+    target_dir=$HOME/Developer/ubelix/puppet-installer/controlrepo/site-modules/profile
     # Create the session and implicitly window (0)
-    tmux -2 new-session -n puppet -s ${SESSION_NAME} -d -c $PUP_DIR
+    tmux -2 new-session -n puppet -s ${session_name} -d -c $target_dir
     # Split window (0) in 4 panes
-    tmux split-window -v -c $PUP_DIR
-    tmux split-window -h -c $PUP_DIR
-    tmux split-window -h -c $PUP_DIR
+    tmux split-window -v -c $target_dir
+    tmux split-window -h -c $target_dir
+    tmux split-window -h -c $target_dir
     tmux select-layout tiled
-    return
+    tmux send-keys -t ${session_name}:1.4 'vagrant up service01 puppet puppetdb01 nfs01 lrms01 submit01 anode001' C-m
+
+    target_dir=$HOME/Developer/ubelix/puppet-installer/controlrepo/modules/puppet-module-slurm
+    tmux new-window -n slurm -t ${session_name} -c $target_dir
+    tmux split-window -v -c $target_dir
+    tmux split-window -h -c $target_dir
+    tmux split-window -h -c $target_dir
+
+    target_dir=$HOME/.dotfiles
+    tmux new-window -n dotfiles -t ${session_name} -c $target_dir
+    tmux split-window -h -c $target_dir
+    tmux split-window -v -c $target_dir
+    tmux select-layout tiled
+
+    target_dir=$HOME
+    tmux new-window -n Hams -t ${session_name} -c $target_dir
+    tmux split-window -v -c $target_dir
+    tmux split-window -h -c $target_dir
+    tmux split-window -h -c $target_dir
+    tmux select-layout tiled
   fi
 
-  tmux -2 new-session -s ${SESSION_NAME} -d
+  tmux -2 new-session -s ${session_name} -d
   # shell (1)
-  #tmux new-window -n bash -t ${SESSION_NAME}
-  #tmux send-keys -t ${SESSION_NAME}:1 'git status' C-m
+  #tmux new-window -n bash -t ${session_name}
+  #tmux send-keys -t ${session_name}:1 'git status' C-m
 
   # mysql (2)
-  #tmux new-window -n mysql -t ${SESSION_NAME}
-  #tmux send-keys -t ${SESSION_NAME}:2 'mysql -u <username> <database>' C-m
+  #tmux new-window -n mysql -t ${session_name}
+  #tmux send-keys -t ${session_name}:2 'mysql -u <username> <database>' C-m
 
   # server/debug log (3)
-  #tmux new-window -n puppet -t ${SESSION_NAME}
-  #tmux send-keys -t ${SESSION_NAME}:3 'bundle exec rails s' C-m
-  #tmux split-window -v -t ${SESSION_NAME}:3
-  #tmux send-keys -t ${SESSION_NAME}:3.1 'tail -f log/development.log | grep "DEBUG"' C-m
+  #tmux new-window -n puppet -t ${session_name}
+  #tmux send-keys -t ${session_name}:3 'bundle exec rails s' C-m
+  #tmux split-window -v -t ${session_name}:3
+  #tmux send-keys -t ${session_name}:3.1 'tail -f log/development.log | grep "DEBUG"' C-m
 
   # rails console (4)
-  #tmux new-window -n console -t ${SESSION_NAME}
-  #tmux send-keys -t ${SESSION_NAME}:4 'pry -r ./config/environment' C-m
+  #tmux new-window -n console -t ${session_name}
+  #tmux send-keys -t ${session_name}:4 'pry -r ./config/environment' C-m
 
   # Start out on the first window when we attach
-  #tmux select-window -t ${SESSION_NAME}:0
+  #tmux select-window -t ${session_name}:0
 fi
-tmux attach -t ${SESSION_NAME}
+tmux attach -t ${session_name}
