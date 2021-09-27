b7351ccafb65b3853e8fa7d33b5af905d5022981
Michael Rolli <michael@rollis.ch>
2021-09-27 10:39:05 +0200

    Adjust to UPN

--- bin/git-fix-id-branding.sh
+++ bin/git-fix-id-branding.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git filter-branch -f --commit-filter 'if [ "$GIT_AUTHOR_NAME" == "Michael Rolli" ]; then export GIT_AUTHOR_NAME="Rolli, Michael (ID)"; export GIT_AUTHOR_EMAIL="michael.rolli@id.unibe.ch"; export GIT_COMMITTER_NAME="Rolli, Michael (ID)"; export GIT_COMMITTER_EMAIL="michael.rolli@id.unibe.ch"; fi; git commit-tree "$@"'
+git filter-branch -f --commit-filter 'if [ "$GIT_AUTHOR_EMAIL" == "michael.rolli@id.unibe.ch" ]; then export GIT_AUTHOR_NAME="Michael Rolli"; export GIT_AUTHOR_EMAIL="michael.rolli@unibe.ch"; export GIT_COMMITTER_NAME="Michael Rolli"; export GIT_COMMITTER_EMAIL="michael.rolli@unibe.ch"; fi; git commit-tree "$@"'
 
