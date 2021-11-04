9be640b8832c1266a0b5e2a39eca47f27cb5f2ce
Michael Rolli <michael@rollis.ch>
2021-11-04 17:53:20 +0100

    Add install script to install nvim config deps

---
+++ neovim/install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Some tools that are used under the hood
+brew install fd ripgrep
+
+# Python 2
+target=/tmp/get-pip.py
+curl -O https://bootstrap.pypa.io/pip/2.7/get-pip.py -o $target
+python2 $target
+rm $target
+python2 -m pip install --upgrade "pip < 21.0"
+python2 -m pip install --user --upgrade pynvim
+
+# Python 3
+brew install python@3.9
+python3 -m pip install --user --upgrade pynvim
+
+# Ruby support
+sudo gem install neovim
+
+# NPM support
+npm install -g neovim
