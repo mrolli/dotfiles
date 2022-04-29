1aba14ee9363023ddf8df9d1c702ce5ea5a74c9c
Michael Rolli <michael@rollis.ch>
2022-04-29 09:16:35 +0200

    Add additional textobjects
    Add new textobjects for classes and methods/functions and commands to
    swap params in signatures.
    
    Fixes #15

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -116,6 +116,69 @@ lvim.builtin.treesitter.ensure_installed = {
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.textobjects = {
+  select = {
+    enable = true,
+    -- Automatically jump forward to textobj, similar to targets.vim
+    lookahead = true,
+
+    keymaps = {
+      -- You can use the capture groups defined in textobjects.scm
+      ["af"] = "@function.outer",
+      ["if"] = "@function.inner",
+      ["ac"] = "@class.outer",
+      ["ic"] = "@class.inner",
+    },
+  },
+  swap = {
+    enable = true,
+    swap_next = {
+      ["<leader>a"] = "@parameter.inner",
+    },
+    swap_previous = {
+      ["<leader>A"] = "@parameter.inner",
+    },
+  },
+  move = {
+    enable = true,
+    set_jumps = true, -- whether to set jumps in the jumplist
+    goto_next_start = {
+      ["]m"] = "@function.outer",
+      ["]["] = "@class.outer",
+    },
+    goto_next_end = {
+      ["]M"] = "@function.outer",
+      ["]]"] = "@class.outer",
+    },
+    goto_previous_start = {
+      ["[m"] = "@function.outer",
+      ["[["] = "@class.outer",
+    },
+    goto_previous_end = {
+      ["[M"] = "@function.outer",
+      ["[]"] = "@class.outer",
+    },
+  },
+  lsp_interop = {
+    enable = true,
+    border = 'none',
+    peek_definition_code = {
+      ["<leader>df"] = "@function.outer",
+      ["<leader>dF"] = "@class.outer",
+    },
+  },
+}
+-- Add lables to the textobjects keybindings
+require("which-key").register({
+  ["[["] = { "Previous class start"},
+  ["[]"] = { "Previous class end"},
+  ["]]"] = { "Next class end"},
+  ["]["] = { "Next class start"},
+  ["<leader>a"] = { "Swap w/next param" },
+  ["<leader>A"] = { "Swap w/prev param" }
+})
+-- lvim.builtin.which_key.mappings["a"] = { "Swap with next param" }
+-- lvim.builtin.which_key.mappings["A"] = { "Swap with prev param" }
 
 -- generic LSP settings
 
@@ -220,6 +283,7 @@ lvim.plugins = {
       require("trouble").setup {}
     end
   },
+  { "nvim-treesitter/nvim-treesitter-textobjects" },
   { "tpope/vim-fugitive" },
   { "tpope/vim-surround" },
   { "rodjek/vim-puppet" },
