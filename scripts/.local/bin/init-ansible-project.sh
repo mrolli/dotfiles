e2e7c1c83cf55fd383ea8771fc89d114e6507d2f
Michael Rolli <michael@rollis.ch>
2023-01-24 08:16:44 +0100

    Fix ansible project initialzation script

--- scripts/.local/bin/init-ansible-project.sh
+++ scripts/.local/bin/init-ansible-project.sh
@@ -67,7 +67,7 @@ ansible_managed = "Managed by ansible, don't make changes here!"
 pipelining = true
 ==================== .envrc ====================
 export ANSIBLE_CONFIG=$(expand_path ansible.cfg)
-export ANSIBLE_FORCE_COLOR: '1'
+export ANSIBLE_FORCE_COLOR=1
 export PY_COLORS=1
 ==================== inventory ====================
 [all:vars]
