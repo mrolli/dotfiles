3ffc170fe6ffa7129bfc357dae83475d53621217
Michael Rolli <michael@rollis.ch>
2023-04-07 06:16:24 +0200

    refactor(zsh): move export to zshenv

--- config/zsh/macos.zsh
+++ config/zsh/macos.zsh
@@ -1,16 +1,16 @@
 #!/usr/bin/env zsh
 # This file contains stuff that is specific for macOS
 
-# User installed packages using OS Make sure installed python packages avallable in PATH
+# Python packages installed using OS Python binary Make sure they are available
+# in PATH
 [ -x "$HOME/Library/Python/3.9/bin" ] && export PATH="$HOME/Library/Python/3.9/bin:$PATH"
 
-# On amd64 Homebrew is now installed in /opt and not automatically available!
+# On armd4 platform Homebrew has a different prefix and is therefore not
+# automatically initialized.
 if ! type brew &>/dev/null && [ -x /opt/homebrew/bin/brew ]
 then
     export PATH="/opt/homebrew/bin:$PATH"
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-# Vagrant/Packer specific
-export PACKER_CACHE_DIR="$HOME/.cache/packer_cache"
 [ -f "$HOME/.vagrant.d/vcloud_token.sh" ] && source "$HOME/.vagrant.d/vcloud_token.sh"
