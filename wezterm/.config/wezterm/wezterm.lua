046e4bec0ecb6cfdbd0bc254839575060659f4ab
Michael Rolli <michael@rollis.ch>
2023-04-16 09:09:16 +0200

    feat(wezterm): add tab navigation using arrow keys

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -37,5 +37,18 @@ config.use_cap_height_to_scale_fallback_fonts = true
 config.hide_tab_bar_if_only_one_tab = true
 config.scrollback_lines = 10000
 
+-- key bindings - https://wezfurlong.org/wezterm/config/keys.html
+config.keys = {
+  {
+    key = 'RightArrow',
+    mods = 'SUPER',
+    action = wezterm.action.ActivateTabRelative(1),
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'SUPER',
+    action = wezterm.action.ActivateTabRelative(-1),
+  },
+}
 -- and finally, return the configuration to wezterm
 return config
