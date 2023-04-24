a3ca3e955c93673a34913622bedf17f849960754
Michael Rolli <michael@rollis.ch>
2023-04-24 06:57:18 +0200

    fix(zsh): fix variable name for resetting the cli color

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -3,12 +3,12 @@
 # For the confirmation moments in life
 prompt_confirm() {
   while true; do
-    quest=$(printf "\r[ ${fg[yellow]}??${reset-color} ] ${1:-Continue?} [y/n]: ")
+    quest=$(printf "\r[ ${fg[yellow]}??${reset_color} ] ${1:-Continue?} [y/n]: ")
     read "reply?$quest"
     case $reply in
       [yY]) echo ; return 0 ;;
       [nN]) echo ; return 1 ;;
-      *) printf " ${fg[red]} %s \n${reset-color}" "invalid input"
+      *) printf " ${fg[red]} %s \n${reset_color}" "invalid input"
     esac
   done
 }
@@ -40,30 +40,30 @@ function append {
 
 # brewup one-liner to keep things up2date
 function brewup {
-  echo "[ ${fg[blue]}..${reset-color} ] Updating Homebrew"
+  echo "[ ${fg[blue]}..${reset_color} ] Updating Homebrew"
   brew update
 
   for type in formulae casks; do
     out=$(brew outdated --$type)
     if [ -n "${out}" ]
     then
-      echo "[ ${fg_no_bold[blue]}..${reset-color} ] The following $type are outdated:"
+      echo "[ ${fg_no_bold[blue]}..${reset_color} ] The following $type are outdated:"
       echo $out
       if [ "${1}" = "-f" ] || prompt_confirm "Shall I upgrade all of them?"
       then
         brew upgrade --$type
-        echo "[ ${fg[blue]}..${reset-color} ] Running brew cleanup"
+        echo "[ ${fg[blue]}..${reset_color} ] Running brew cleanup"
         brew cleanup
       fi
     fi
   done
 
-  echo "[ ${fg[blue]}..${reset-color} ] Running brew doctor"
+  echo "[ ${fg[blue]}..${reset_color} ] Running brew doctor"
   brew doctor
 
   if [ "${1}" = "-f" ] || prompt_confirm "Shall I upgrade AppStore apps?"
   then
-    echo "[ ${fg[blue]}..${reset-color} ] Running mas upgrade"
+    echo "[ ${fg[blue]}..${reset_color} ] Running mas upgrade"
     mas upgrade
   fi
 }
