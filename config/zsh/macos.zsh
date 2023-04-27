ca4e4df70b93d8f612fa50a16c84434164798d2c
Michael Rolli <michael@rollis.ch>
2023-04-27 07:26:23 +0200

    refacotr(zsh): move handling of brew to macos.zsh

--- config/zsh/macos.zsh
+++ config/zsh/macos.zsh
@@ -5,12 +5,14 @@
 # in PATH
 [ -x "$HOME/Library/Python/3.9/bin" ] && export PATH="$HOME/Library/Python/3.9/bin:$PATH"
 
-# On armd4 platform Homebrew has a different prefix and is therefore not
-# automatically initialized.
-if ! type brew &>/dev/null && [ -x /opt/homebrew/bin/brew ]
+# On arm64 platform Homebrew is not automatically in PATH
+if ! commanmd -v brew &>/dev/null && [ -x /opt/homebrew/bin/brew ]
 then
     export PATH="/opt/homebrew/bin:$PATH"
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
+
+# Initialize Homebrew for all platforms here
+command -v brew &>/dev/null && eval "$(brew shellenv)"
 
 [ -f "$HOME/.vagrant.d/vcloud_token.sh" ] && source "$HOME/.vagrant.d/vcloud_token.sh"
