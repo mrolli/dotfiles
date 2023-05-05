4eed37c066eaea6a1aa2d482682f833a6cbf2474
Michael Rolli <michael@rollis.ch>
2023-05-05 11:28:13 +0200

    feat(nvim): disable animations again
    This didn't help for me.

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
