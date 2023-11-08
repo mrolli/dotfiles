dc586615bb043b744d1db7462f8afda8cbfe4b23
Michael Rolli <michael@rollis.ch>
2023-11-08 05:58:00 +0100

    feat: map moving lines to symbols

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -23,6 +23,14 @@ map("n", "n", "nzzzv")
 map("n", "N", "Nzzzv")
 map("n", "gd", "gdzzzv")
 
+-- Move Lines
+-- map("n", "∆", "<cmd>m .+1<cr>==", { desc = "Move down" })
+-- map("n", "˚", "<cmd>m .-2<cr>==", { desc = "Move up" })
+-- map("i", "∆", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+-- map("i", "˚", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+-- map("v", "∆", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+-- map("v", "˚", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+
 --[[
   Using leader + y/Y to yank to unnameplus or paste from unnamedplus register.
   The register unnamedplus is the system's clipboard and is the default
