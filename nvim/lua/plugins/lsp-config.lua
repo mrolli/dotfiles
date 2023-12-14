a1dfb66b9e64b11cb1f25e2b3353b13435f8d100
Michael Rolli <michael@rollis.ch>
2023-12-14 06:08:01 +0100

    feat(nvim): re-add lsp-config overrides

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,61 @@
+-- LSP related addtional configuration
+
+local diag_format = function(d)
+  local code = d.code or (d.user_data and d.user_data.lsp.code)
+  if code then
+    return string.format("%s (%s)", d.message, code):gsub("1. ", "")
+  end
+  return d.message
+end
+
+return {
+  -- Automatically intall addditional language server,
+  -- debug adapters, linters and formatters
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        -- language servers
+        "ansible-language-server",
+        "bash-language-server",
+        "lua-language-server",
+        "yaml-language-server",
+        -- debug adapters
+        -- linters
+        "ansible-lint",
+        "shellcheck",
+        -- formatters
+      })
+    end,
+  },
+
+  -- Configure changes to LSP servers
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- -@type lspconfig.options
+      -- Show error code in virtual text output
+      virtual_text = {
+        format = diag_format,
+      },
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                disable = { "lowercase-global" },
+              },
+            },
+          },
+        },
+        yamlls = {
+          settings = {
+            yaml = {
+              keyOrdering = false,
+            },
+          },
+        },
+      },
+    },
+  },
+}
