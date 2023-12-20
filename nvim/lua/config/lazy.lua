30af1485dea850dc1ccf27a625d3a8877f576486
Michael Rolli <michael@rollis.ch>
2023-12-20 13:53:12 +0100

    refactor(nvim): set colorscheme when configuring LazyVim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -9,7 +9,9 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
+      colorscheme = "gruvbox-material",
+    } },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.editor.navic" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -19,9 +19,6 @@ return {
       -- vim.g.gruvbox_material_diagnostic_line_highlight = 1
       vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
       -- vim.g.gruvbox_material_better_performance = 1
-
-      -- now load the colorscheme here
-      vim.cmd([[colorscheme gruvbox-material]])
     end,
   },
 }
