4c7ea4e2221cd0a5950ae1d53790ff4585a396f4
Michael Rolli <michael@rollis.ch>
2024-10-06 11:25:27 +0200

    feat: cleanup completion

--- config/zsh/completion.zsh
+++ config/zsh/completion.zsh
@@ -11,44 +11,44 @@ fi
 
 autoload -U compinit && compinit -d $ZCACHEDIR/zcompdump
 
-# _comp_options+=(globdots) # with hidden files
-# zstyle ':completion:*' completer _extensions _complete
-# #zstyle ':completion:*' completer _extensions _complete _approximate
-# zstyle ':completion:*' menu select
-# zstyle ':completion:*' use-cache on
-# zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
-# zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
-# zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
-# zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
-# zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
-# zstyle ':completion:*' group-name ''
-# zstyle ':completion:*:*:-command-:*:*' group-order alias builtins functions commands
-# zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-# zstyle ':completion:*' squeeze-slashes true
-# zstyle ':completion:*' complete-options true
+_comp_options+=(globdots) # with hidden files
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+zstyle ':completion:*' completer _extensions _complete
+zstyle ':completion:*' menu select
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
+zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*:*:-command-:*:*' group-order alias builtins functions commands
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' squeeze-slashes true
+zstyle ':completion:*' complete-options true
 
 # Activate 1password autocompletion if 1password is avaialble
-# if command -v op &>/dev/null; then
-#   eval "$(op completion zsh)"; compdef _op op
-# fi
+if command -v op &>/dev/null; then
+  eval "$(op completion zsh)"; compdef _op op
+fi
 
 # Activate wezterm autocompletion if wezterm is avaialble
-# if command -v wezterm &>/dev/null; then
-#   eval "$(wezterm shell-completion --shell zsh)"
-# fi
+if command -v wezterm &>/dev/null; then
+  eval "$(wezterm shell-completion --shell zsh)"
+fi
 
 # Activate azure autocompletion if az is avaialble
-# if command -v az &>/dev/null; then
-#   autoload bashcompinit && bashcompinit
-#   source $(brew --prefix)/etc/bash_completion.d/az
-# fi
+if command -v az &>/dev/null; then
+  autoload bashcompinit && bashcompinit
+  source $(brew --prefix)/etc/bash_completion.d/az
+fi
 
 # Activate terraform autocompletion if terraform is avaialble
-# if command -v terraform &>/dev/null; then
-#   complete -C "$(command -v terraform)" terraform
-# fi
+if command -v terraform &>/dev/null; then
+  complete -C "$(command -v terraform)" terraform
+fi
 
 # Activate packer autocompletion if packer is avaialble
-# if command -v packer &>/dev/null; then
-#   complete -C "$(command -v packer)" packer
-# fi
+if command -v packer &>/dev/null; then
+  complete -C "$(command -v packer)" packer
+fi
