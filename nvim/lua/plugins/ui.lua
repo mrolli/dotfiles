4dcb8566f1485272259565051383126160d0c439
Michael Rolli <michael@rollis.ch>
2024-10-11 09:59:30 +0200

    feat(nvim): add virtual column plugin

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,12 @@
+return {
+  -- Setup virt-column.nvim by lukas-reineke
+  -- see https://github.com/lukas-reineke/virt-column.nvim
+  -- see :help virt-column.txt
+  {
+    "lukas-reineke/virt-column.nvim",
+    opts = {
+      char = { "┆" },
+      virtcolumn = "80",
+    },
+  },
+}
