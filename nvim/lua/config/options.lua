44105880101635111252ef91d05f25d099b0bd64
Michael Rolli <michael@rollis.ch>
2024-10-11 10:05:18 +0200

    feat(nvim): add spelling for de again

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- no automatic system clipboard usage
+-- see also keybindings regarding yanking
+-- vim.opt.clipboard = ""
+
+-- turn off formatting on save
+-- vim.g.autoformat = false
+
+-- configure spellcheck to use both english and german
+vim.opt.spelllang = { "en", "de" }
