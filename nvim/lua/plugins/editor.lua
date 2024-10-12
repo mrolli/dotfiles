c8c3ac58bc4b1aa58562b275d69c38fab7e6c9ab
Michael Rolli <michael@rollis.ch>
2024-10-12 21:14:20 +0200

    feat(nvim): re-add colorizer plugin again

---
+++ nvim/lua/plugins/editor.lua
@@ -0,0 +1,24 @@
+return {
+  -- see https://github.com/NvChad/nvim-colorizer.lua
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      filetypes = {
+        "*",
+        "!gitcommit",
+      },
+      user_default_options = {
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        -- Available modes: foreground, background, virtualtext
+        mode = "virtualtext", -- Set the display mode.
+        virtualtext = "■",
+      },
+    },
+  },
+}
