eafb793741a5eb5ad34cebe66c746c2f629b254e
Michael Rolli <michael@rollis.ch>
2024-10-12 17:33:43 +0200

    feat(nvim): re-add zen-mode and twilight

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,5 +1,4 @@
 return {
-  -- Setup virt-column.nvim by lukas-reineke
   -- see https://github.com/lukas-reineke/virt-column.nvim
   -- see :help virt-column.txt
   {
@@ -10,12 +9,54 @@ return {
     },
   },
   -- Modify core plugin bufferline
+  -- see :help bufferline-configuration
   {
     "akinsho/bufferline.nvim",
     opts = {
       options = {
         show_buffer_close_icons = false,
         show_close_icon = false,
+      },
+    },
+  },
+  -- see https://github.com/folke/zen-mode.nvim
+  {
+    "folke/zen-mode.nvim",
+    keys = {
+      { "<leader>uz", "<cmd>ZenMode<cr>", desc = "Toggle ZenMode" },
+    },
+    opts = {
+      window = {
+        options = {
+          number = true,
+          relativenumber = false,
+        },
+      },
+      plugins = {
+        twilight = { enabled = true },
+        -- this will change the font size on wezterm when in zen mode
+        -- See alse also the Plugins/Wezterm section in this projects README
+        wezterm = {
+          enabled = true,
+          -- can be either an absolute font size or the number of incremental steps
+          font = "+4", -- (10% increase per step)
+        },
+      },
+    },
+  },
+  -- see https://github.com/folke/twilight.nvim
+  {
+    "folke/twilight.nvim",
+    keys = {
+      { "<leader>ut", "<cmd>Twilight<cr>", desc = "Toggle Twilight" },
+    },
+    opts = {
+      context = 8,
+      expand = {
+        "function",
+        "method",
+        "block",
+        "if_statement",
       },
     },
   },
