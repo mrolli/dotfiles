e45c046e910b76044600acb70854361a4cd587c8
Michael Rolli <michael@rollis.ch>
2024-10-12 17:34:14 +0200

    feat(nvim): add plugin incline.nvim
    Having filenames on panes seems to be a good idea even if we have the
    bufferline.

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -8,6 +8,27 @@ return {
       virtcolumn = "80",
     },
   },
+  {
+    "b0o/incline.nvim",
+    config = function()
+      require("incline").setup({
+        -- window = { margin = { vertical = 0, horizontal = 1 } },
+        -- hide = { cursorline = true, only_win = true },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[*]" .. filename
+          end
+
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
+    -- Optional: Lazy load Incline
+    event = "VeryLazy",
+  },
   -- Modify core plugin bufferline
   -- see :help bufferline-configuration
   {
