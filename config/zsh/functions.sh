9e82c535ab18df3f6067ff4dbf0268ad2b3cd3cc
Michael Rolli <michael@rollis.ch>
2024-12-16 09:21:10 +0100

    feat(zsh): add some Azure related convenience functions

--- config/zsh/functions.sh
+++ config/zsh/functions.sh
@@ -1,14 +1,13 @@
 # Function definitions
 
 # Open manpages in terminalapp
-function manx { open x-man-page://$@ ; }
+function manx { open x-man-page://$@; }
 # Open mangpages as PDF in preview app
-function manp { man -t $@ | open -f -a "Preview" ; }
+function manp { man -t $@ | open -f -a "Preview"; }
 
 # Prepend a string to a filename
 function prepend {
-  if [ $# -ne 2 ]
-  then
+  if [ $# -ne 2 ]; then
     echo "Usage: ${0} FILE PREFIX"
   else
     mv "${1}" "${2}${1}"
@@ -17,8 +16,7 @@ function prepend {
 
 # Append a sring to a filename
 function append {
-  if [ $# -ne 2 ]
-  then
+  if [ $# -ne 2 ]; then
     echo "Usage: ${0} FILE SUFFIX"
   else
     mv "${1}" "${1}${2}"
@@ -27,8 +25,7 @@ function append {
 
 # print week number based on date or now()
 function week {
-  if [ "${1}" = "-h" ]
-  then
+  if [ "${1}" = "-h" ]; then
     echo "Usage: week [YYYY-MM-DD]"
     return
   fi
@@ -42,13 +39,12 @@ function week {
 
 # print SSL certificate of remote website
 function https-cert {
-  if [ $# -ne 1 ]
-  then
+  if [ $# -ne 1 ]; then
     echo "Usage: https-cert HOST"
     return
   fi
 
-  openssl s_client -showcerts -servername "${1}" -connect "${1}":443 < /dev/null 2>/dev/null | openssl x509 -inform pem -noout -text
+  openssl s_client -showcerts -servername "${1}" -connect "${1}":443 </dev/null 2>/dev/null | openssl x509 -inform pem -noout -text
 }
 
 # load a local tmuxp config file in a directory by first changing pwd
@@ -68,4 +64,37 @@ function tmuxpl {
   else
      echo "Error: Provided path is not a readable directory path" && return 2
   fi
+}
+
+# Check for valid Azure CLI session
+function __has_az_ready {
+  if ! command -v az &>/dev/null; then
+    echo "Azure CLI not found. Please install it first."
+    return 1
+  fi
+
+  if ! az account show &>/dev/null; then
+    echo "No Azure CLI session found. Please run 'az login' first."
+    return 1
+  fi
+}
+
+# Switch between Azure subscriptions using fzf
+function azsubswitch {
+  __has_az_ready || return 1
+
+  if ! command -v fzf &>/dev/null; then
+    echo "fzf not found. Please install it first."
+    return 1
+  fi
+
+  az account list --query "[].{id:id,name:name}" -o tsv |
+    fzf --no-multi --bind 'enter:execute(az account set -s {1})+abort'
+}
+
+# List available Azure subscriptions
+function azsublist {
+  __has_az_ready || return 1
+
+  az account list --query "[].{name:name, subscriptionId:id}" -o table
 }
