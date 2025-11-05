cbbb530aa658b78aba175ea52aba3964cc2198fe
Michael Rolli <michael@rollis.ch>
2025-11-05 11:49:41 +0100

    feat(nvim): add spelllang and autoformat options again

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,6 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- turn off formatting on save
+-- vim.g.autoformat = false
+
+-- set spellcheck languages to English and Swiss German
+vim.o.spelllang = "en,de_ch"
+
 -- add support for bicep and bicepparam files
 vim.filetype.add({
   extension = {
