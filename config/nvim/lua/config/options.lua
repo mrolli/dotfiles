2b7cd182aa62f1e83f015a9fce49b6b16cbdd28a
Michael Rolli <michael@rollis.ch>
2025-11-06 14:39:34 +0100

    feat(nvim): add old keymaps again although mostly commented
    Decide later if activating yanky is beneficial or going back to use
    unamedplus explicitly again.

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- no automatic system clipboard usage
+-- see also keybindings regarding yanking
+-- vim.opt.clipboard = ""
+
 -- turn off formatting on save
 -- vim.g.autoformat = false
 
