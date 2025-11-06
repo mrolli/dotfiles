0eadea2ee4b8b43b6bcbcfe601ece4200c06720e
Michael Rolli <michael@rollis.ch>
2025-11-06 15:11:25 +0100

    feat(nvim): add some autocommands again

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -13,6 +13,34 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   command = "set commentstring=//\\ %s",
 })
 
+-- Automatically set text width for markdown files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown" },
+  command = "set tw=80",
+})
+
+-- Empty formatexpr for markdwon files to make gqq and gqap work again
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown", "gitcommit" },
+  command = "set formatexpr=",
+})
+
+-- Some filetypes should have wrapping enabled by default
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "json" },
+  -- enable wrap mode
+  command = "setlocal wrap",
+})
+
+-- Disable the concealing in some file formats
+-- The default conceallevel is 3 in LazyVim
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = { "json", "jsonc" },
+--   callback = function()
+--     vim.opt.conceallevel = 0
+--   end,
+-- })
+
 -- Set relative line numbers when not in insert mode
 vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" }, {
   pattern = "*",
