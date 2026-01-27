3eafedc13a2475abfcf57778a3c519dc529cc93a
Michael Rolli <michael@rollis.ch>
2026-01-27 09:12:15 +0100

    feat(nvim): add a global mardownlint configuration for now

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -11,4 +11,14 @@ return {
       },
     },
   },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          prepend_args = { "--config", vim.env.HOME .. "/.config/nvim/.markdownlint.yaml", "--" },
+        },
+      },
+    },
+  },
 }
